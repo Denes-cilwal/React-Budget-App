d7b9c9663de40354ac8913c0145f2c5418d93bb8
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-23 17:46:47 +0545

    add uuid package

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-import {ExpenseList} from "./components/ExpenseList"
-import {Alert} from "./components/Alert"
-import {ExpenseTrackerForm} from "./components/ExpenseTrackerForm";
+import "./App.css";
+import { ExpenseList } from "./components/ExpenseList";
+import { Alert } from "./components/Alert";
+import { ExpenseTrackerForm } from "./components/ExpenseTrackerForm";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const InitialExpenses = [
+  { id:uuidv4(), charge: "rent", amount: 1600 },
+  { id:uuidv4(), charge: "car payment", amount: 1600 },
+  { id:uuidv4(), charge: "credit card bill", amount: 1600 },
+];
+
+console.log(InitialExpenses)
 
 function App() {
   return (
-      <>
-     <ExpenseList/>
-     <Alert/>
-     <ExpenseTrackerForm/>
-     </>
+    <>
+      <ExpenseList />
+      <Alert />
+      <ExpenseTrackerForm />
+    </>
   );
 }
 
