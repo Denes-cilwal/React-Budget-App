0fcb9fe48c13823872592072ad6248a395ae3c38
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-23 18:42:40 +0545

    added button icon and reacticons package

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -2,23 +2,37 @@ import "./App.css";
 import { ExpenseList } from "./components/ExpenseList";
 import { Alert } from "./components/Alert";
 import { ExpenseTrackerForm } from "./components/ExpenseTrackerForm";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 const InitialExpenses = [
-  { id:uuidv4(), charge: "rent", amount: 1600 },
-  { id:uuidv4(), charge: "car payment", amount: 1600 },
-  { id:uuidv4(), charge: "credit card bill", amount: 1600 },
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 1600 },
+  { id: uuidv4(), charge: "credit card bill", amount: 1600 },
 ];
 
-console.log(InitialExpenses)
+console.log(InitialExpenses);
 
 function App() {
+  const [expenses, setExpense] = useState(InitialExpenses);
   return (
     <>
-      <ExpenseList />
       <Alert />
-      <ExpenseTrackerForm />
+      <h1>Budget Calculating App</h1>
+      <main className="App">
+        <ExpenseTrackerForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        {/* acc is total and curr is current item in iteration */}
+        total resultAmount:
+        <span className="total">
+          ${" "}
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
 }
--- calculator-app/src/components/ExpenseList.js
+++ calculator-app/src/components/ExpenseList.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import {ItemExpense} from "./ItemExpense";
+import {MdClear} from "react-icons/md";
 
-export const ExpenseList = () => {
+export const ExpenseList = ({expenses}) => {
     return (
-        <div>
-            <ItemExpense/>
-        </div>
+        <>
+        <ul className="list">
+        {
+            expenses.map((expense) =>{
+                // get key and remaining data ...
+                return <ItemExpense key={expense.id} expense={expense}/>
+            })
+        }
+        </ul>
+        {expenses.length > 0 && 
+        <button 
+        className="btn"
+        >
+        clear expense <MdClear className="btn-icon"/>
+        </button>}
+        </>
     )
 }
